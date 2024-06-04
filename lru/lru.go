d422a1739f6efd53b098614809e0ad089d45c369
nullptroot <1611576808@qq.com>
2024-06-04 23:19:41 +0800

    实现了lru模块

---
+++ lru/lru.go
@@ -0,0 +1,79 @@
+package lru
+
+import "container/list"
+
+type Cache struct {
+	maxBytes int64
+	nbytes   int64
+	ll       *list.List
+	cache    map[string]*list.Element
+
+	OnEvicted func(key string, value Value)
+}
+
+// cache中存的条目
+type entry struct {
+	key   string
+	value Value
+}
+
+// cache要存的值，只需要实现这个接口即可
+type Value interface {
+	Len() int
+}
+
+// 创建新的cache对象
+func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+}
+
+// 向cache中添加kv，若已经存在，那么就调整
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		// 新添加的value和原来的value尺寸可能不一样
+		c.nbytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		c.nbytes += int64(len(key)) + int64(value.Len())
+	}
+	for c.maxBytes != 0 && c.maxBytes < c.nbytes {
+		c.RemoveOldest()
+	}
+}
+
+// 获取kv cache，并调整
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
+
+// 空间不够时 淘汰末尾的cache
+func (c *Cache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+func (c *Cache) Len() int {
+	return c.ll.Len()
+}
